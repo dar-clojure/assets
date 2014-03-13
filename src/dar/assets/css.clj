1553c0b8f8b13adc039db052f5d226d8953fce69
Eldar Gabdullin <eldargab@gmail.com>
2014-03-13 12:15:28 +0400

    rename :styles -> :css

--- src/dar/assets/css.clj
+++ src/dar/assets/css.clj
@@ -3,7 +3,7 @@
 
 (defn build [env]
   (let [files (for [pkg (:packages env)
-                    file (:styles pkg)]
+                    file (:css pkg)]
                 (let [url (get-url pkg file)
                       p (resource-path pkg file)
                       out (target env p)]

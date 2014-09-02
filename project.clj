50a0ba0bafabfe74921181ea8948fc034c7a39b2
Eldar Gabdullin <eldargab@gmail.com>
2014-09-02 21:09:41 +0400

    bump ClojureScript

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject dar/assets "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]]
+                 [org.clojure/clojurescript "0.0-2322"]]
   :target-path "build/target"
   :clean-targets ["build"])

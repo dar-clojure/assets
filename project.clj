b023f7a727faf02bc01c7e36df6c75e179bd964d
Eldar Gabdullin <eldargab@gmail.com>
2014-10-12 19:45:34 +0400

    Release 0.3.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject dar/assets "0.0.2"
+(defproject dar/assets "0.0.3"
   :description "Web resources management framework"
   :url "https://github.com/dar-clojure/assets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2342"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [dar/container "0.2.0"]
                  [hiccup "1.0.5"]]
   :profiles {:dev {:source-paths ["examples"]}}
